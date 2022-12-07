af4d1b6f265cf1bab3f83a803d52e2d3adbfa124
Puspita <puspitaanggraeni769@gmail.com>
2022-12-07 18:41:39 +0700

    using formatter

--- scripts/customer_analysis.py
+++ scripts/customer_analysis.py
@@ -1,96 +1,128 @@
-#import library
+# import library
 import pandas as pd
 import datetime
 import altair as alt
 
 from pyodide.http import open_url
 
-#get dataset
+# get dataset
 url = "https://raw.githubusercontent.com/puspitamelati/3K-Analysis/main/3K_Data.csv?token=GHSAT0AAAAAAB3TEWTV6654JYPIQAJHIUQWY37QDRA"
-df = pd.read_csv(open_url(url),parse_dates=["Date"])
+df = pd.read_csv(open_url(url), parse_dates=["Date"])
 
-#cleaning data
-df.drop('Unnamed: 0', axis=1, inplace=True)
+# cleaning data
+df.drop("Unnamed: 0", axis=1, inplace=True)
 df = df.dropna()
-df['Date'] = pd.to_datetime(df['Date'])
-df['Year'] = df['Date'].dt.to_period('Y').astype(str)
-df['Quantity'] = df['Quantity'].astype(int)
-df['Price'] = df['Price'].astype(float)
-df['Customer ID'] = df['Customer ID'].astype(str)
-df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-df['Date'] = pd.to_datetime(df['Date'])
-df['Sales'] = (df['Price'])*(df['Quantity'])
-df = df[df['Quantity']>=0]
+df["Date"] = pd.to_datetime(df["Date"])
+df["Year"] = df["Date"].dt.to_period("Y").astype(str)
+df["Quantity"] = df["Quantity"].astype(int)
+df["Price"] = df["Price"].astype(float)
+df["Customer ID"] = df["Customer ID"].astype(str)
+df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
+df["Date"] = pd.to_datetime(df["Date"])
+df["Sales"] = (df["Price"]) * (df["Quantity"])
+df = df[df["Quantity"] >= 0]
 
-#make datafram grouped by year and Customer ID
-df_group_2 = df.groupby(['Date', 'Customer ID', 'Year']
-            ).agg({'Price' : 'mean', 'Quantity' : 'sum', 'Sales' : 'sum'}
-            ).reset_index()
+# make datafram grouped by year and Customer ID
+df_group_2 = (
+    df.groupby(["Date", "Customer ID", "Year"])
+    .agg({"Price": "mean", "Quantity": "sum", "Sales": "sum"})
+    .reset_index()
+)
 
-#add bin and selection
-ad_bin = alt.binding_select(options=['2016', '2017', '2018', '2020', '2021'], name='Year')
-ad_selec = alt.selection_single(fields=['Year'], bind=ad_bin)
+# add bin and selection
+ad_bin = alt.binding_select(
+    options=["2016", "2017", "2018", "2020", "2021"], name="Year"
+)
+ad_selec = alt.selection_single(fields=["Year"], bind=ad_bin)
 
-#Top 5 Best Customer
-group_2 = df.groupby(['Year','Customer ID']
-            ).agg({'Quantity' : 'sum', 'Sales' : 'sum'}
-            ).reset_index()
-group_2['Rank'] = group_2.groupby(['Year'])['Quantity'].rank(ascending=False, method='first')
+# Top 5 Best Customer
+group_2 = (
+    df.groupby(["Year", "Customer ID"])
+    .agg({"Quantity": "sum", "Sales": "sum"})
+    .reset_index()
+)
+group_2["Rank"] = group_2.groupby(["Year"])["Quantity"].rank(
+    ascending=False, method="first"
+)
 
-best = alt.Chart(group_2).mark_bar(color='maroon').encode(
-    alt.X('sum(Quantity):Q',
-         axis = alt.Axis(title = 'Date')),
-    alt.Y('Customer ID:O', sort ='-x',
-        axis = alt.Axis(title = 'Customer ID', titleColor='maroon')),
-).add_selection(ad_selec
-).transform_filter(
-    ad_selec
-).transform_filter(
-    alt.FieldRangePredicate(field='Rank', range=[1,5]))
+best = (
+    alt.Chart(group_2)
+    .mark_bar(color="maroon")
+    .encode(
+        alt.X("sum(Quantity):Q", axis=alt.Axis(title="Date")),
+        alt.Y(
+            "Customer ID:O",
+            sort="-x",
+            axis=alt.Axis(title="Customer ID", titleColor="maroon"),
+        ),
+    )
+    .properties(title="Top 5 Best Customer", height=300, width=400)
+    .add_selection(ad_selec)
+    .transform_filter(ad_selec)
+    .transform_filter(alt.FieldRangePredicate(field="Rank", range=[1, 5]))
+)
 
-#total customer across time
-g_1 = alt.Chart(df_group_2).mark_bar(color='maroon').encode(
-    alt.X('yearmonth(Date):T',
-         axis = alt.Axis(title = 'Date')),
-    alt.Y('distinct(Customer ID):O',
-        axis = alt.Axis(title = 'Total Customer', titleColor='maroon')),
-    #alt.Color('Quantity:N', 
-              #legend = alt.Legend(title = 'Basket Size')),
-              #bin = alt.Bin(step=5)), #using (N) to see how many item that purchases from each customer
-    tooltip = ('yearmonth(Date):T', 'distinct(Customer ID):N')
-).properties(title='Total Customer Accros Month ',
-    height= 300, width = 300
-).add_selection(ad_selec
-).transform_filter(
-    ad_selec)
+# total customer across time
+g_1 = (
+    alt.Chart(df_group_2)
+    .mark_bar(color="maroon")
+    .encode(
+        alt.X("yearmonth(Date):T", axis=alt.Axis(title="Date")),
+        alt.Y(
+            "distinct(Customer ID):O",
+            axis=alt.Axis(title="Total Customer", titleColor="maroon"),
+        ),
+        # alt.Color('Quantity:N',
+        # legend = alt.Legend(title = 'Basket Size')),
+        # bin = alt.Bin(step=5)), #using (N) to see how many item that purchases from each customer
+        tooltip=("yearmonth(Date):T", "distinct(Customer ID):N"),
+    )
+    .properties(title="Total Customer Accros Month ", height=300, width=400)
+    .add_selection(ad_selec)
+    .transform_filter(ad_selec)
+)
 
-#see new customer accros month
-group_cust = df_group_2.drop_duplicates(subset=['Customer ID'], keep='first') #new dataset that have not duplicatet customer id
+# see new customer accros month
+group_cust = df_group_2.drop_duplicates(
+    subset=["Customer ID"], keep="first"
+)  # new dataset that have not duplicatet customer id
 
-g_2 = alt.Chart(group_cust).mark_bar(color='maroon', interpolate='monotone').encode(
-    alt.X('yearmonth(Date):T',
-         axis = alt.Axis(title = 'Date')),
-    alt.Y('count(Customer ID):O',
-        axis = alt.Axis(title = 'Total New Customer', titleColor='maroon')),
-    tooltip = ('yearmonth(Date):T', 'count(Customer ID):N')
-).properties(title='New Customer Accros Month ',
-    height= 300, width = 300
-).add_selection(ad_selec
-).transform_filter(
-    ad_selec)
+g_2 = (
+    alt.Chart(group_cust)
+    .mark_bar(color="maroon", interpolate="monotone")
+    .encode(
+        alt.X("yearmonth(Date):T", axis=alt.Axis(title="Date")),
+        alt.Y(
+            "count(Customer ID):O",
+            axis=alt.Axis(title="Total New Customer", titleColor="maroon"),
+        ),
+        tooltip=("yearmonth(Date):T", "count(Customer ID):N"),
+    )
+    .properties(title="New Customer Accros Month ", height=300, width=400)
+    .add_selection(ad_selec)
+    .transform_filter(ad_selec)
+)
 
-#average curtomer basket size across months
-g_3 = alt.Chart(df_group_2).transform_aggregate(
-    monthly_count= 'count(Quantity):Q',
-    groupby=['Date', 'Year']).mark_bar(color='maroon').encode(
-    alt.X('yearmonth(Date):T',
-         axis = alt.Axis(title = 'Date')),
-    alt.Y('average(monthly_count):Q',
-        axis = alt.Axis(title = 'Average Basket Size', format='.1f', titleColor='maroon')),
-    tooltip = ('yearmonth(Date):T','average(monthly_count):Q' )
-).properties(title='Average Customer Basket Size Accros Month ',
-    height= 300, width = 300
-).add_selection(ad_selec
-).transform_filter(ad_selec)
+# average curtomer basket size across months
+g_3 = (
+    alt.Chart(df_group_2)
+    .transform_aggregate(monthly_count="count(Quantity):Q", groupby=["Date", "Year"])
+    .mark_bar(color="maroon")
+    .encode(
+        alt.X("yearmonth(Date):T", axis=alt.Axis(title="Date")),
+        alt.Y(
+            "average(monthly_count):Q",
+            axis=alt.Axis(
+                title="Average Basket Size", format=".1f", titleColor="maroon"
+            ),
+        ),
+        tooltip=("yearmonth(Date):T", "average(monthly_count):Q"),
+    )
+    .properties(
+        title="Average Customer Basket Size Accros Month ", height=300, width=400
+    )
+    .add_selection(ad_selec)
+    .transform_filter(ad_selec)
+)
 
-alt.vconcat(best|g_1 & g_2 & g_3 )
+alt.vconcat(best & g_1 | g_2 & g_3)
--- scripts/product_sum.py
+++ scripts/product_sum.py
@@ -1,44 +1,54 @@
-#import library
+# import library
 import pandas as pd
 import datetime
 import altair as alt
 from pyodide.http import open_url
 
-#get dataset
+# get dataset
 url = "https://raw.githubusercontent.com/puspitamelati/3K-Analysis/main/3K_Data.csv?token=GHSAT0AAAAAAB3TEWTV6654JYPIQAJHIUQWY37QDRA"
-df = pd.read_csv(open_url(url),parse_dates=["Date"])
+df = pd.read_csv(open_url(url), parse_dates=["Date"])
 
 
-#cleaning data
-df.drop('Unnamed: 0', axis=1, inplace=True)
+# cleaning data
+df.drop("Unnamed: 0", axis=1, inplace=True)
 df = df.dropna()
-df['Date'] = pd.to_datetime(df['Date'])
-df['Year'] = df['Date'].dt.to_period('Y').astype(str)
-df['Quantity'] = df['Quantity'].astype(int)
-df['Price'] = df['Price'].astype(float)
-df['Customer ID'] = df['Customer ID'].astype(str)
-df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-df['Date'] = pd.to_datetime(df['Date'])
-df['Sales'] = (df['Price'])*(df['Quantity'])
-df = df[df['Quantity']>=0]
+df["Date"] = pd.to_datetime(df["Date"])
+df["Year"] = df["Date"].dt.to_period("Y").astype(str)
+df["Quantity"] = df["Quantity"].astype(int)
+df["Price"] = df["Price"].astype(float)
+df["Customer ID"] = df["Customer ID"].astype(str)
+df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
+df["Date"] = pd.to_datetime(df["Date"])
+df["Sales"] = (df["Price"]) * (df["Quantity"])
+df = df[df["Quantity"] >= 0]
 
-#make dataframe groupby year and item name
-group_1 = df.groupby(['Year','Item Name']).agg({'Quantity' : 'sum', 'Sales' : 'sum'}).reset_index()
+# make dataframe groupby year and item name
+group_1 = (
+    df.groupby(["Year", "Item Name"])
+    .agg({"Quantity": "sum", "Sales": "sum"})
+    .reset_index()
+)
 
-#make dataframe that grouped by Date, Year, and Item Name
-df_group_1 = df.groupby(['Date','Item Name','Year']).agg({'Price' : 'mean', 'Quantity' : 'sum', 'Sales' : 'sum'}).reset_index()
+# make dataframe that grouped by Date, Year, and Item Name
+df_group_1 = (
+    df.groupby(["Date", "Item Name", "Year"])
+    .agg({"Price": "mean", "Quantity": "sum", "Sales": "sum"})
+    .reset_index()
+)
 
 
-#ranked the quantity each year
-group_1['Rank_1']=group_1.groupby(['Year'])['Quantity'].rank(ascending=False)
+# ranked the quantity each year
+group_1["Rank_1"] = group_1.groupby(["Year"])["Quantity"].rank(ascending=False)
 
-#add bin and selection
-pts = alt.selection_multi(encodings=['y'])
-ad_bin = alt.binding_select(options=['2016', '2017', '2018', '2020', '2021'], name='Year')
-ad_selec = alt.selection_single(fields=['Year'], bind=ad_bin)
+# add bin and selection
+pts = alt.selection_multi(encodings=["y"])
+ad_bin = alt.binding_select(
+    options=["2016", "2017", "2018", "2020", "2021"], name="Year"
+)
+ad_selec = alt.selection_single(fields=["Year"], bind=ad_bin)
 
-#Purchased Product
-'''
+# Purchased Product
+"""
 prod = alt.Chart(group_1).mark_bar(color='maroon').encode(
     alt.X('sum(Quantity):Q',
          axis = alt.Axis(title = 'Date')),
@@ -48,54 +58,73 @@ prod = alt.Chart(group_1).mark_bar(color='maroon').encode(
 ).add_selection(ad_selec
 ).transform_filter(
     ad_selec)
-'''
+"""
 
-#Best Selling product
-best = alt.Chart(group_1).mark_bar(color='maroon').encode(
-    alt.X('sum(Quantity):Q',
-        axis = alt.Axis(title = 'Total Sold')),
-    alt.Y('Item Name:O', sort = '-x',
-        axis = alt.Axis(title = 'Product Name', titleColor='maroon')),
-    color = alt.condition(pts, alt.value('maroon'), alt.value("grey"))
-).add_selection(ad_selec
-).add_selection(pts
-).transform_filter(
-    ad_selec
-).transform_filter(
-    alt.FieldRangePredicate(field='Rank_1', range=[1,10])
+# Best Selling product
+best = (
+    alt.Chart(group_1)
+    .mark_bar(color="maroon")
+    .encode(
+        alt.X("sum(Quantity):Q", axis=alt.Axis(title="Total Sold")),
+        alt.Y(
+            "Item Name:O",
+            sort="-x",
+            axis=alt.Axis(title="Product Name", titleColor="maroon"),
+        ),
+        color=alt.condition(pts, alt.value("maroon"), alt.value("grey")),
+    )
+    .add_selection(ad_selec)
+    .add_selection(pts)
+    .transform_filter(ad_selec)
+    .transform_filter(alt.FieldRangePredicate(field="Rank_1", range=[1, 10]))
 )
 
-#Total Sales across months
-g_1 = alt.Chart(df_group_1).mark_bar(color='maroon').encode(
-    alt.X('yearmonth(Date):T',
-          axis = alt.Axis(title = 'Year', tickCount=5)),
-    alt.Y('sum(Sales):Q',
-         axis = alt.Axis(title = 'Total Sales', labelColor='maroon', titleColor='maroon')),
-    tooltip = ('yearmonth(Date):T', 'sum(Sales)'))
+# Total Sales across months
+g_1 = (
+    alt.Chart(df_group_1)
+    .mark_bar(color="maroon")
+    .encode(
+        alt.X("yearmonth(Date):T", axis=alt.Axis(title="Year", tickCount=5)),
+        alt.Y(
+            "sum(Sales):Q",
+            axis=alt.Axis(
+                title="Total Sales", labelColor="maroon", titleColor="maroon"
+            ),
+        ),
+        tooltip=("yearmonth(Date):T", "sum(Sales)"),
+    )
+)
 
 
-#Total sales across months
-g_2 = alt.Chart(df_group_1).mark_line(color='black', interpolate='monotone', strokeDash=[10, 10]).encode(
-    alt.X('yearmonth(Date):T',
-        axis = alt.Axis(title = 'Date', tickCount=5)),
-    alt.Y('sum(Quantity):Q',
-        axis = alt.Axis(title = 'Total Items Purchased', labelColor='black', titleColor='black')),
-    tooltip = ('yearmonth(Date):T', 'sum(Quantity):Q', 'sum(Sales)'))
+# Total sales across months
+g_2 = (
+    alt.Chart(df_group_1)
+    .mark_line(color="black", interpolate="monotone", strokeDash=[10, 10])
+    .encode(
+        alt.X("yearmonth(Date):T", axis=alt.Axis(title="Date", tickCount=5)),
+        alt.Y(
+            "sum(Quantity):Q",
+            axis=alt.Axis(
+                title="Total Items Purchased", labelColor="black", titleColor="black"
+            ),
+        ),
+        tooltip=("yearmonth(Date):T", "sum(Quantity):Q", "sum(Sales)"),
+    )
+)
 
-point_1 = g_2.mark_circle(color='black').encode(
-    opacity = alt.value(1))
+point_1 = g_2.mark_circle(color="black").encode(opacity=alt.value(1))
 
-#combine Total Sales and total Items Purchased 
-sales_purch = alt.layer(g_1, g_2+point_1).resolve_scale(
-    y = 'independent' 
-).properties(
-    title = "Total Sales and Total Items Purchased Across Months",
-).add_selection(ad_selec
-).transform_filter(
-    ad_selec
-).add_selection(pts
-).transform_filter(
-    pts)
+# combine Total Sales and total Items Purchased
+sales_purch = (
+    alt.layer(g_1, g_2 + point_1)
+    .resolve_scale(y="independent")
+    .properties(
+        title="Total Sales and Total Items Purchased Across Months",
+    )
+    .add_selection(ad_selec)
+    .transform_filter(ad_selec)
+    .add_selection(pts)
+    .transform_filter(pts)
+)
 
 best | sales_purch
-
